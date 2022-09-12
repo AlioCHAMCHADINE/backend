f9c597b7283b410d6560139c5cb0fe914781635a
Alio CHAMCHADINE <chamsadine.alio@gmail.com>
2022-09-12 20:30:46 +0200

    nodemon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du premier serveur !');
+    res.end('Voilà la réponse du prémier serveur !');
 });
 
-server.listen(process.env.PORT || 3000); 
+server.listen(process.env.PORT || 3000);        
